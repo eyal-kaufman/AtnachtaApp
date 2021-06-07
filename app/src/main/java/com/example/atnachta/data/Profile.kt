ff0a35369a3a81220386d0e2c5e08df9eebd822e
eyal-kaufman <59081215+eyal-kaufman@users.noreply.github.com>
2021-06-07 12:22:21 +0300

    neww

--- app/src/main/java/com/example/atnachta/data/Profile.kt
+++ app/src/main/java/com/example/atnachta/data/Profile.kt
@@ -1,10 +1,50 @@
 package com.example.atnachta.data
 
+import java.time.LocalDate
+
+
 class Profile {
+//    Personal Details:
     var firstName : String? = null
     var lastName : String? = null
-    lateinit var searchList : List<String?> // TODO eyal - keep this field
     var phone : String? = null
+    var ID : String = ""
+    var homeAddress : String = ""
+    var dateOfBirth : LocalDate? = null
+    var originCountry : String = ""
+    var yearOfAliyah : String = ""
+    var religiosity : String = ""
+    var citizenshipStatus : String = ""
+//    var age : Int = 0
+//    Education:
+    var lastStudyingAt : String = ""
+    var grade : String = ""
+    var isActiveStudent : String = ""
+//    Health:
+    var medicalCare : String = ""
+    var medication : String = ""
+    var medicalProblems : String = ""
+//    Father:
+    var fatherName : String = ""
+    var fatherAddress : String = ""
+    var fatherPhone : String = ""
+    var fatherStatus : String = ""
+//    Mother:
+    var motherName : String = ""
+    var motherAddress : String = ""
+    var motherPhone : String = ""
+    var motherStatus : String = ""
+//    Other guardian:
+    var isOtherGuardian : Boolean = false
+    var guardianName : String = ""
+    var guardianAddress : String = ""
+    var guardianPhone : String = ""
+    var guardianStatus : String = ""
+
+
+
+    lateinit var searchList : List<String?> // TODO eyal - keep this field
+
 
     constructor() {}
     constructor(firstName: String?, lastName: String?, phone: String?) {
